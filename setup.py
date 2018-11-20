3b5f84caf172127eaaec7114e4c304722bffe0cc
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2018-11-20 12:05:44 -0300

    Improve setup and add LICENSE

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
-from distutils.core import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 requires = [i.strip() for i in open("requirements.txt").readlines()]
 
-setup(name='pagarme-surfmappers',
-      version='1.0',
-      description='Simple Pagar.me module made by Surfmappers.com',
-      author='Yuri Alessandro Martins',
-      author_email='yuri@surfmappers.com',
-      url='',
-      install_requires=requires,)
+setuptools.setup(
+    name='pagarme-surfmappers',
+    version='1.0',
+    description='Simple Pagar.me module made by Surfmappers.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Yuri Alessandro Martins',
+    author_email='yuri@surfmappers.com',
+    url='https://github.com/YuriAlessandro/pagarme-surfmappers',
+    install_requires=requires,
+    packages=setuptools.find_packages(),
+    classifiers=[
+         "Programming Language :: Python :: 3",
+    ],
+)
