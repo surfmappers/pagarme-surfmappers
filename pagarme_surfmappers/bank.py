02a8e717aa619b914f20f1d5183bb5cea93411ae
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2018-11-22 10:11:26 -0300

    Add Bank URL to constants file; Create BankAccount class

---
+++ pagarme_surfmappers/bank.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import constants
+import utils
+
+
+class BankAccount(object):
+    """docstring for BankAccount"""
+    def __init__(self):
+        super(BankAccount, self).__init__()
