37f6725dd01b5133a925a2d22298ae935b4f1376
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2018-11-26 10:32:24 -0300

    Change project verion

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requires = [i.strip() for i in open("requirements.txt").readlines()]
 
 setuptools.setup(
     name='pagarme-surfmappers',
-    version='1.0',
+    version='1.2.0',
     description='Simple Pagar.me module made by Surfmappers.com',
     long_description=long_description,
     long_description_content_type="text/markdown",
